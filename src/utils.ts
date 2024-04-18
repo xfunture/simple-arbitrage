894a23b0c895093c8da36597e935bfbbc7712d7d
xfunture <15768184216@163.com>
2024-04-18 18:29:46 +0800

    first commit

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,8 @@ export function bigNumberToDecimal(value: BigNumber, base = 18): number {
   return value.mul(10000).div(divisor).toNumber() / 10000
 }
 
+//在没有设置FLASHBOTS_RELAY_SIGNING_KEY的情况下，随机创建一个钱包，返回钱包中的私钥
+//该私钥用于在FlashBots 内部的声誉系统中作为唯一对的身份标识
 export function getDefaultRelaySigningKey(): string {
   console.warn("You have not specified an explicity FLASHBOTS_RELAY_SIGNING_KEY environment variable. Creating random signing key, this searcher will not be building a reputation for next run")
   return Wallet.createRandom().privateKey;
